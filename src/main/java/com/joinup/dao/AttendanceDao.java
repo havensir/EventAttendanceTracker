6feffca27cee11ecce3e94fa69c060ebddfcac6b
sidsiddu <85114685+sidsiddu@users.noreply.github.com>
2025-10-31 20:39:14 -0400

    Delete src/main/java/com/joinup/dao/AttendanceDao.java

--- src/main/java/com/joinup/dao/AttendanceDao.java
+++
@@ -1,6 +0,0 @@
-try {
-    preparedStatement.executeUpdate();
-} catch (SQLException e) {
-    logger.error("Failed to execute update for attendee", e);
-    throw new RuntimeException("Database error occurred", e);
-}
